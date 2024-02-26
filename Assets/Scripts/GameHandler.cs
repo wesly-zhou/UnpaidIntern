fa9f84f636d3ee2e627079f1105d387cecba64d0
wesly-zhou <weslyz1026@gmail.com>
2024-02-26 11:17:50 -0500

    Finished pause menu, can transition between scenes appropriately

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,23 +7,8 @@ using UnityEngine.SceneManagement;
 public class GameHandler : MonoBehaviour
 {
 
-    public GameObject scoreText;
-    private static int playerScore = 0;
-
-    void Start()
-    {
-        UpdateScore();
-    }
-
     public void StartGame()
     {
-        playerScore = 0;
-        SceneManager.LoadScene("LevelOne");
-    }
-
-    public void Resume()
-    {
-        playerScore = 0;
         SceneManager.LoadScene("LevelOne");
     }
 
@@ -34,8 +19,8 @@ public class GameHandler : MonoBehaviour
 
     public void RestartGame()
     {
-        // Time.timeScale = 1f;
-        // GameHandler_PauseMenu.GameisPaused = false;
+        Time.timeScale = 1f;
+        GameHandler_PauseMenu.GameisPaused = false;
         MainMenu();
     }
 
@@ -51,25 +36,6 @@ public class GameHandler : MonoBehaviour
         #else
                 Application.Quit(); 
         #endif
-    }
-
-    public void AddScore(int points)
-    {
-        playerScore += points;
-        UpdateScore();
-    }
-
-    void UpdateScore()
-    {
-        Text scoreTextB = scoreText.GetComponent<Text>();
-        if (SceneManager.GetActiveScene().name == "EndScene")
-        {
-            scoreTextB.text = "FINAL SCORE: " + playerScore;
-        }
-        else
-        {
-            scoreTextB.text = "SCORE: " + playerScore;
-        }
     }
 
 }
